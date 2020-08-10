0680102f8e63618914fc16d98bec974347819043
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-10 17:06:47 -0400

    Add global style. Style spinner

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,14 @@ import React from 'react';
 import Header from './elements/Header';
 import Home from './Home';
 
-const App = () => <div><Header/><Home/></div>;
+import { GlobalStyle } from './styles/GlobalStyle';
+
+const App = () => (
+  <>
+    <Header />
+    <Home />
+    <GlobalStyle />
+  </>
+)
 
 export default App;
