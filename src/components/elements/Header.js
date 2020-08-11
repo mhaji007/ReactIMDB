ee91d63f01e3a37367cbf5f4d4e2a5c51a8831e0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-11 19:23:26 -0400

    Add route to movie detail page

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from '@reach/router';
 
 import RMDBLogo from '../images/reactMovie_logo.png';
 import TMDBLogo from '../images/tmdb_logo.svg';
@@ -9,11 +10,16 @@ import {
     StyledTMDBLogo
 } from '../styles/StyledHeader';
 
+// 1. Learn how to create a styled basic styled component
+// 2. Learn how to handle props in styled component
+// 3. Create a global style with styled components
 
 const Header = () => (
 <StyledHeader>
   <div className="header-content">
-    <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
+    <Link to="/">
+      <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
+    </Link>
     <StyledTMDBLogo src={TMDBLogo} alt="tmdb-logo" />
   </div>
 </StyledHeader>
